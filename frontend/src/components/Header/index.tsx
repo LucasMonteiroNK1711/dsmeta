7f8ca266732d603d4977c29f6a34c5da99a7869b
Lucas Monteiro <joselucasmonteiro@gmail.com>
2022-07-11 16:27:45 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import logo from '../../assets/img/logo.svg'
+import './styles.css'
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="DSMeta" />
+                <h1>DSMeta</h1>
+                <p>Desenvolvido por
+                    <a href="https://www.linkedin.com/in/josé-lucas-monteiro-santos-367067210/" target="_blank">José Lucas
+                        Monteiro</a>
+                </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
